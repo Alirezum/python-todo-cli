b0828c5bd9d3c7ed6fa345384803455d017d0ecd
alireza <alirezueffati@gmail.com>
2025-11-11 12:25:43 +0330

    __main__ and main simple function

--- src/__main__.py
+++ src/__main__.py
@@ -1,6 +1,4 @@
-def main():
-    print("Hello from todo-cli!")
-
+from src.cli.main import app
 
 if __name__ == "__main__":
-    main()
+    app()
--- src/cli/main.py
+++ src/cli/main.py
@@ -1,1 +1,12 @@
+import typer
 
+from src.models.category import Category
+from src.models.task import Task
+from src.models.tag import Tag
+
+app = typer.Typer()
+
+
+@app.command()
+def hello(name, str):
+    print(f"hi {name}")
